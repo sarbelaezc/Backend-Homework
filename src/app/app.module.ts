8270de6f95a6098d775e0bdebbf4cddfa0d38c5a
jofdiazdi <jofdiazdi@unal.edu.co>
2018-09-01 14:44:50 -0500

    Patch Mongoose connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './controllers/app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers :[
