4f1d90521532983a200cd3a3624b8b0b9000c22b
jofdiazdi <jofdiazdi@unal.edu.co>
2018-09-01 14:27:50 -0500

    MongooseConnection Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { Module } from '@nestjs/common';
 @Module({
   controllers :[
     AppController
-  ]
+  ],
+  imports: [
+    MongooseModule.forRoot('mongodb://<admin>:<admin18>@ds233212.mlab.com:33212/noteschallenge')
+  ],
 })
 export class AppModule {}
