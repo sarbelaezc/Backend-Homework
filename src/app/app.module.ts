b1ffe6fe8bb0a233545c6b350e85e7740cb6a7e6
Jofdiazdi <jofdiazdi@unal.edu.co>
2018-09-01 14:54:10 -0500

    Mongoose connection (#6)
    * NestJS controller added
    
    * Update app.controller.ts
    
    * MongooseConnection Added
    
    * Patch Mongoose connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './controllers/app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers :[
     AppController
+  ],
+  imports: [
+    MongooseModule.forRoot('mongodb://<admin>:<admin18>@ds233212.mlab.com:33212/noteschallenge')
   ]
 })
 export class AppModule {}
