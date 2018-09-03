08dd98ff29c1e8d0a6d82effe07fc8e801f1b555
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-09-03 17:32:08 -0500

    fixed some file names

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,9 @@
 import * as mongoose from 'mongoose';
-import { DB_PROVIDER } from '../constants';
+// import { DB_PROVIDER } from '../constants';
 
 export const databaseProviders = [
     {
-        provide: DB_PROVIDER,
+        provide: 'DbConnectionToken',
         useFactory: async () => {
             (mongoose as any).Promise = global.Promise;
             return await mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds141972.mlab.com:41972/backend-homework');
