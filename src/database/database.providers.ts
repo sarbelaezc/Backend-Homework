c2255222739770120059a2909ef74b51740c0f2c
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-09-03 19:04:24 -0500

    fixed some db conection problems

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,12 +1,10 @@
 import * as mongoose from 'mongoose';
-// import { DB_PROVIDER } from '../constants';
+import { DB_PROVIDER } from '../constants';
 
 export const databaseProviders = [
-    {
-        provide: 'DbConnectionToken',
-        useFactory: async () => {
-            (mongoose as any).Promise = global.Promise;
-            return await mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds141972.mlab.com:41972/backend-homework');
-        },
-    },
+  {
+    provide: DB_PROVIDER,
+    useFactory: async (): Promise<typeof mongoose> =>
+        await mongoose.connect('mongodb://admin:admin123@ds141972.mlab.com:41972/backend-homework',{ useNewUrlParser: true }),
+  },
 ];
