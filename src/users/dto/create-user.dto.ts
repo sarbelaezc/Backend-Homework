ea2b89771d2a36a6eed83220f76f6410e9912ea8
jofdiazdi <jofdiazdi@unal.edu.co>
2018-09-05 14:34:56 -0500

    Notes Model added to the users
    Please test this

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,6 @@
+import { CreateNoteDto } from '.../notes/dto/create-note.dto.ts';
 export class CreateUserDto {
     readonly username: string;
     readonly name: string;
-    // readonly notes: [NoteSchema];
+    readonly notes: [CreateNoteDto];
 }
--- src/users/interfaces/user.interface.ts
+++ src/users/interfaces/user.interface.ts
@@ -1,7 +1,7 @@
 import { Document } from 'mongoose';
-
+import { Note as NoteInterface } from '.../notes/interface/note.interface.ts';
 export interface User extends Document {
   readonly username: string;
   readonly name: string;
-  // readonly notes: [NoteSchema];
+  readonly notes: [NoteInterface];
 }
