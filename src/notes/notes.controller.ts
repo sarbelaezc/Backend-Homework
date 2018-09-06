8c186ff83abbcdc2ebf516f4b103b835fb29cf74
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-09-05 23:44:10 -0500

    Added OpenAPI and some responses

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -2,23 +2,31 @@ import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateNotesDto } from './dto/create-note.dto';
 import { NotesService} from './notes.service';
 import { Note as NoteInterface } from './interfaces/note.interface.ts';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('notes')
 export class NotesController{
   constructor(private readonly notesService: NotesService){
+
     @Post()
+    @ApiResponse({ status: 201, description: 'The note has been successfully created' })
     async create(@Body() createNoteDto: CreateNoteDto ){
       this.notesService.create(createNoteDto);
     }
+
     @Get()
     async findAll(): Promise<NoteInterface[]>{
       return this.notesService.findAll();
     }
+
     @Post()
+    @ApiResponse({ status: 201, description: 'The note has been successfully updated' })
     async update(id,@Body() text:string): Promise<NoteInterface>{
       return this.notesService.update(id,text);
     }
+
     @Post()
+    @ApiResponse({ status: 201, description: 'The note has been successfully deleted' })
     async delete(id){
       this.notesService.delete(id);
     }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,11 @@
 import { CreateNoteDto } from '.../notes/dto/create-note.dto.ts';
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+    @ApiModelProperty()
     readonly username: string;
+    @ApiModelProperty()
     readonly name: string;
+    @ApiModelProperty()
     readonly notes: [CreateNoteDto];
 }
